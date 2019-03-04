d0276b43ff3c573c096e7b44d95078c971a37dc0
Craig Riley Donavin <crdonavin@gmail.com>
2019-03-04 19:44:43 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,14 +1,14 @@
 def unsafe?(speed)
   if speed > 60
-    return true
+     true
   elsif speed <40
-    return true
-  else return false
+     true
+  else  false
 end
 
 
 
 def not_safe?(speed)
-  speed<40 ?  true :  false
+  speed<40 ? return true : return false
 end
 end
